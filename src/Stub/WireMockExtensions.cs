77f5ae853944d417f88bf6b17c1d587068d622a7
Tim Peel <tim.peel@equalexperts.com>
2025-05-14 07:45:11 +0100

    Add first iteration of stubs (#2)
    * Add initial stubs from test data scenarios
    
    * Stub based on the scenarios present
    
    * Fix created, updated date
    
    * Fix created, updated and timestamp for related CDS data
    
    * Add finalisation PHA scenario
    
    * Improve pipeline
    
    * Enforce GBTEEP1 point of entry

---
+++ src/Stub/WireMockExtensions.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace Defra.TradeImportsDataApiStub.Stub;
+
+[ExcludeFromCodeCoverage]
+public static class WireMockExtensions
+{
+    private static Type Anchor => typeof(WireMockExtensions);
+
+    public static void StubAllScenarios<T>(this WireMockServer wireMock, ILogger<T> logger)
+    {
+        var scenarios = GetAllScenariosStubs();
+
+        foreach (var scenario in scenarios)
+        {
+            var path = scenario.Replace("_", "/").Replace(".json", "");
+            var response = Response
+                .Create()
+                .WithStatusCode(StatusCodes.Status200OK)
+                .WithBody(GetBody(scenario));
+            var request = Request.Create().WithPath(path).UsingGet();
+
+            wireMock.Given(request).RespondWith(response);
+
+            logger.LogInformation("Stubbed {Scenario}", path);
+        }
+    }
+
+    private static IEnumerable<string> GetAllScenariosStubs() =>
+        Anchor
+            .Assembly.GetManifestResourceNames()
+            .Where(x => x.StartsWith($"{GetScenarioPrefix()}"))
+            .Select(x => x.Replace(GetScenarioPrefix(), ""));
+
+    private static string GetBody(string fileName)
+    {
+        using var stream = Anchor.Assembly.GetManifestResourceStream(
+            $"{GetScenarioPrefix()}{fileName}"
+        );
+
+        if (stream is null)
+            throw new InvalidOperationException($"Unable to find embedded resource {fileName}");
+
+        using var reader = new StreamReader(stream);
+
+        return reader.ReadToEnd();
+    }
+
+    private static string GetScenarioPrefix() => $"{Anchor.Namespace}.Scenarios.";
+}
